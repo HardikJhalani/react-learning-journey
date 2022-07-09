17408d58810461348c77591c81c0dbb54d8587ce
Hardik Jhalani <hardikjhalani23092001@gmail.com>
2022-07-09 16:23:40 +0530

    learnt javascript expressions in JSX

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'
+import './index.css';
+
+const name = 'Hardik Jhalani';
 
 ReactDOM.render(
-  <h1>Hello guys</h1>,
+  <>
+    <h1>Hello guys, my name is {name}</h1>
+    <p>My lucky number is {10 + 13}</p>
+  </>,
   document.getElementById('root')
 );
